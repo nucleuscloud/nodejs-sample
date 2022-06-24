515d720a07728e9a25f681eea267b943478b1ed3
Nick Z <2420177+nickzelei@users.noreply.github.com>
2022-06-24 11:41:51 -0700

    updated sample to use env var

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const http = require('http');
+const url = require('url');
 
 const port = process.env.PORT || 3000;
 
-http.createServer(function (_req, res) {
+http.createServer(function (req, res) {
+    const queryObject = url.parse(req.url, true).query;
+    let name = process.env.DEFAULT_NAME ?? 'Anonymous';
+    if (queryObject.name) {
+      name = queryObject.name;
+    }
+
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.end(`Hello ${name}\n`);
 }).listen(port, "0.0.0.0", () => {
   console.log(`Server running at http://0.0.0.0:${port}/`);
 });
